ddd8cb7f0c607d07cc472aab51dfabaeab8f0819
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-14 19:51:18 -0800

    refactored <a> tags into <Link> tags

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function GalleryItem(props) {
     let [view, setView] = useState(false);
@@ -35,10 +36,10 @@ function GalleryItem(props) {
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>
-                    <a href={`/artist/${props.item.artistId}`}>{props.item.artistName}</a>
+                    <Link to={`/artist/${props.item.artistId}`}>{props.item.artistName}</Link>
                 </h3>
                 <h3>
-                    <a href={`/album/${props.item.collectionId}`}>{props.item.collectionName}</a>
+                    <Link href={`/album/${props.item.collectionId}`}>{props.item.collectionName}</Link>
                 </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
