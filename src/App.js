163b5e2c8bf4f1891da85ae182387519b8d97afe
Chris <117781333+chrism-la@users.noreply.github.com>
2024-01-14 18:25:48 -0800

    refactored App component to utilize react-router-dom

--- src/App.js
+++ src/App.js
@@ -35,11 +35,22 @@ function App() {
 
     return (
         <div>
-            <SearchBar handleSearch={handleSearch} />
             {message}
-            <Gallery data={data} />
-            <AlbumView />
-            <ArtistView />
+            <Router>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Fragment>
+                                <SearchBar handleSearch={handleSearch} />
+                                <Gallery data={data} />
+                            </Fragment>
+                        }
+                    />
+                    <Route path="/album/:id" element={<AlbumView />} />
+                    <Route path="/artist/:id" element={<ArtistView />} />
+                </Routes>
+            </Router>
         </div>
     );
 }
